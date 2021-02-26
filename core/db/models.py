f94aa0db0e807539fc7387d40f5ad916b375e686
felipeisj <felipeslzar@gmail.com>
2021-02-26 11:49:06 -0300

    modelo bd actualizado con ondelete

--- core/db/models.py
+++ core/db/models.py
@@ -36,38 +36,41 @@ class Usuario(Base):
 class Muestra(Base):
     fecha = Column(DateTime())
     path = Column(UnicodeText())
-    usuario_id = ForeignKey(Usuario)
+    usuario_id = ForeignKey(Usuario, {'ondelete': 'restrict'}, {'nullable': False})
     usuario = relationship('Usuario', backref=backref('muestras'))
+    tipo_muestra = Column(String(200))
 
 class Celula(Base):
-    muestra_id = ForeignKey(Muestra)
+    muestra_id = ForeignKey(Muestra, {'ondelete': 'restrict'}, {'nullable': False})
     nombre = Column(String(200))
     muestra = relationship('Muestra', backref=backref('celulas'))
     path = Column(String(200))
+    etiquetas = Column(String(200))
 
 class Categoria(Base):
     nombre = Column(String(200))
-    dependencia_id = Column(Integer)
+    dependencia_id = ForeignKey(Categoria, {'ondelete': 'restrict'}, {'nullable': False})
     alteracion = Column(Boolean, default = False)
     descripcion = Column(String(200))
     ejemplo = Column(String(200))
 
 class Etiqueta(Base):
-    categoria_id = ForeignKey(Categoria)
+    categoria_id = ForeignKey(Categoria, {'ondelete': 'restrict'}, {'nullable': False})
     nombre = Column(String(200))
     ejemplo = Column(String(200))
     descripcion = Column(String(400))
+    valores_etiquetas = relationship('ValorEtiqueta', backref=backref('valor_etiquetas'))
 
 class ValorEtiqueta(Base):
-    celula_id = ForeignKey(Celula)
+    celula_id = ForeignKey(Celula, {'ondelete': 'restrict'}, {'nullable': False})
     celula = relationship('Celula', backref=backref('valor_etiquetas'))
-    etiqueta_id = ForeignKey(Etiqueta)
-    etiqueta = relationship('Etiqueta', backref=backref('valor_etiquetas'))
+    etiqueta_id = ForeignKey(Etiqueta, {'ondelete': 'restrict'}, {'nullable': False})
     valor = Column(String(200))
     validacion = Column(Boolean, default=False)
+    respuesta_id = ForeignKey(Respuesta, {'ondelete': 'restrict'}, {'nullable': False})
 
 class Respuesta(Base):
-     usuario_id = ForeignKey(Usuario)
+     usuario_id = ForeignKey(Usuario, {'ondelete': 'restrict'}, {'nullable': False})
      fecha = Column(DateTime())
 
 
